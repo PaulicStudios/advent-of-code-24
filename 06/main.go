b94963633de7adca19afa9d77ce5f73dd4677865
Paul Großmann <git@paulgrossmann.de>
2024-12-06 07:33:44 +0100

    day 06 - part 02 - cursed infinite loop recognition

--- 06/main.go
+++ 06/main.go
@@ -5,9 +5,7 @@ import (
 	"os"
 )
 
-var mapp [][]int32
-
-func parseInputFile() {
+func parseInputFile() [][]int32 {
 	file, err := os.Open("06/input.txt")
 	if err != nil {
 		panic(err)
@@ -15,6 +13,7 @@ func parseInputFile() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	mapp := make([][]int32, 0)
 	for scanner.Scan() {
 		line := scanner.Text()
 
@@ -24,22 +23,24 @@ func parseInputFile() {
 		}
 		mapp = append(mapp, intLine)
 	}
+
+	return mapp
 }
 
-func executeStep(row, col int) (int, int) {
-	switch mapp[row][col] {
+func executeStep(row, col int, mapp *[][]int32) (int, int) {
+	switch (*mapp)[row][col] {
 	case '^':
 		if row == 0 {
 			return -1, -1
 		}
 		return row - 1, col
 	case '>':
-		if col == len(mapp[row])-1 {
+		if col == len((*mapp)[row])-1 {
 			return -1, -1
 		}
 		return row, col + 1
 	case 'v':
-		if row == len(mapp)-1 {
+		if row == len(*mapp)-1 {
 			return -1, -1
 		}
 		return row + 1, col
@@ -66,8 +67,8 @@ func turnRight(direction int32) int32 {
 	return 0
 }
 
-func startPos() (int, int) {
-	for indRow, row := range mapp {
+func startPos(mapp *[][]int32) (int, int) {
+	for indRow, row := range *mapp {
 		for indCol, col := range row {
 			if col == '^' {
 				return indRow, indCol
@@ -77,28 +78,52 @@ func startPos() (int, int) {
 	return -1, -1
 }
 
-func walkLoop() {
-	rowInd, colInd := startPos()
+func walkLoop(mapp *[][]int32) bool {
+	rowInd, colInd := startPos(mapp)
+	if rowInd == -1 || colInd == -1 {
+		return false
+	}
+	initRow, initCol := rowInd, colInd
+	initFirstRow, initFirstCol := -1, -1
+
+	count := 0
+	countAgain := 0
+
 	for {
-		newRow, newCol := executeStep(rowInd, colInd)
+		newRow, newCol := executeStep(rowInd, colInd, mapp)
 		if newRow == -1 || newCol == -1 {
 			break
 		}
+		if initFirstRow == -1 && initFirstCol == -1 {
+			initFirstRow, initFirstCol = newRow, newCol
+		}
 
-		obstruction := mapp[newRow][newCol]
+		if newRow == initFirstRow && newCol == initFirstCol && rowInd == initRow && colInd == initCol {
+			count++
+			if count == 2 {
+				return true
+			}
+		}
+
+		obstruction := (*mapp)[newRow][newCol]
 		if obstruction == '#' {
-			mapp[rowInd][colInd] = turnRight(mapp[rowInd][colInd])
+			(*mapp)[rowInd][colInd] = turnRight((*mapp)[rowInd][colInd])
 		} else {
-			mapp[newRow][newCol] = mapp[rowInd][colInd]
-			mapp[rowInd][colInd] = 'X'
+			(*mapp)[newRow][newCol] = (*mapp)[rowInd][colInd]
+			(*mapp)[rowInd][colInd] = 'X'
 			rowInd, colInd = newRow, newCol
 		}
+		countAgain++
+		if countAgain > 10000 {
+			return true
+		}
 	}
+	return false
 }
 
-func countVisited() int {
+func countVisited(mapp *[][]int32) int {
 	count := 1
-	for _, row := range mapp {
+	for _, row := range *mapp {
 		for _, col := range row {
 			if col == 'X' {
 				count++
@@ -108,9 +133,35 @@ func countVisited() int {
 	return count
 }
 
-func main() {
-	parseInputFile()
+func infiniteLoop(mapp [][]int32) int {
+	infiniteLoopMapCount := 0
 
-	walkLoop()
-	println("Part 1: ", countVisited())
+	copyMapp := make([][]int32, len(mapp))
+	for ind, _ := range mapp {
+		copyMapp[ind] = make([]int32, len(mapp[ind]))
+	}
+
+	for row := 0; row < len(mapp); row++ {
+		for col := 0; col < len(mapp[row]); col++ {
+			for ind, _ := range mapp {
+				copy(copyMapp[ind], mapp[ind])
+			}
+
+			copyMapp[row][col] = '#'
+			if walkLoop(&copyMapp) {
+				println("Infinite loop at row: ", row, " col: ", col)
+				infiniteLoopMapCount++
+			}
+		}
+	}
+	return infiniteLoopMapCount
+}
+
+func main() {
+	mapp := parseInputFile()
+
+	println("Part 2: ", infiniteLoop(mapp))
+
+	//walkLoop(&mapp)
+	//println("Part 1: ", countVisited(&mapp))
 }
