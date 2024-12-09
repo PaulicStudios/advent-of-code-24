7ad513bb50ba5555bb06f82fd8d41ea3e995d9f7
Paul Großmann <git@paulgrossmann.de>
2024-12-09 08:43:35 +0100

    day 09 - part 02 - but scuffed solution for part 01

--- 09/main.go
+++ 09/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"adventofcode24/utils"
 	"bufio"
 	"os"
 )
 
 func parseInputFile() []int {
-	file, err := os.Open("09/input.txt")
+	file, err := os.Open("09/test.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -22,7 +23,7 @@ func parseInputFile() []int {
 		line := scanner.Text()
 		data := make([]int, 0)
 		for _, char := range line {
-			data = append(data, int(char-'0'))
+			data = append(data, utils.ConvertToInt(string(char)))
 		}
 		return data
 	}
@@ -30,24 +31,37 @@ func parseInputFile() []int {
 	return []int{}
 }
 
-func blockString(data []int) []int {
-	block := make([]int, 0)
-	//idnbr := make([]int, len(data))
+func blockString(data []int) [][]int {
+	block := make([][]int, 0)
+
 	for id, char := range data {
+		if char == 0 {
+			continue
+		}
 		if id%2 == 0 {
+			innerBlock := make([]int, 0)
 			for iter := 0; iter < char; iter++ {
-				block = append(block, id/2)
+				innerBlock = append(innerBlock, id/2)
 			}
+			block = append(block, innerBlock)
 		} else {
+			innerBlock := make([]int, 0)
 			for iter := 0; iter < char; iter++ {
-				block = append(block, -1)
+				innerBlock = append(innerBlock, -1)
 			}
+			block = append(block, innerBlock)
 		}
 	}
 	return block
 }
 
-func filesToLeft(data []int) []int {
+func filesToLeft(dataNew [][]int) [][]int {
+	data := make([]int, 0)
+	for _, innerBlock := range dataNew {
+		for _, char := range innerBlock {
+			data = append(data, char)
+		}
+	}
 	for indRev := len(data) - 1; indRev >= 0; indRev-- {
 		if data[indRev] == -1 {
 			continue
@@ -61,16 +75,57 @@ func filesToLeft(data []int) []int {
 		}
 	}
 
+	return [][]int{data}
+}
+
+func filesToLeftBlock(data [][]int) [][]int {
+	for indRev := len(data) - 1; indRev >= 0; indRev-- {
+		if data[indRev][0] == -1 {
+			continue
+		}
+		spaceNeeded := len(data[indRev])
+		for ind := 0; ind < indRev; ind++ {
+			spaceAvailable := 0
+			startInd := 0
+			for innerInd, char := range data[ind] {
+				if char == -1 {
+					spaceAvailable++
+					if spaceAvailable >= spaceNeeded {
+						revStrInd := 0
+						for i := startInd; i < startInd+spaceNeeded; i++ {
+							data[ind][i] = data[indRev][revStrInd]
+							data[indRev][revStrInd] = -1
+							revStrInd++
+						}
+						startInd = -1
+						break
+					}
+				} else {
+					spaceAvailable = 0
+					startInd = innerInd + 1
+				}
+			}
+			if startInd == -1 {
+				break
+			}
+		}
+	}
+
 	return data
 }
 
-func checksum(data []int) int {
+func checksum(data [][]int) int {
 	sum := 0
-	for ind, char := range data {
-		if char == -1 {
-			continue
+	ind := 0
+	for _, char := range data {
+		for _, innerChar := range char {
+			if innerChar == -1 {
+				ind++
+				continue
+			}
+			sum += ind * innerChar
+			ind++
 		}
-		sum += char * ind
 	}
 	return sum
 }
@@ -79,4 +134,5 @@ func main() {
 	data := parseInputFile()
 
 	println("Part 1: ", checksum(filesToLeft(blockString(data))))
+	println("Part 2: ", checksum(filesToLeftBlock(blockString(data))))
 }
